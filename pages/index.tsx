2567551075efd86386d6476fabebe5ba9cba88a0
algomachine007 <amaben202phantom@gmail.com>
2022-09-25 00:09:06 +0100

    infering gssp

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
-  return <div>hey</div>;
+import type { InferGetStaticPropsType } from 'next';
+
+export default function Home({
+  products,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return <div>{products}</div>;
+}
+
+export async function getStaticProps() {
+  const products = [1, 2, 4];
+  return {
+    props: {
+      products,
+    },
+    revalidate: 4 * 60 * 60, // every 4hrs in production, chack if there's new data in the database and update
+  };
 }
