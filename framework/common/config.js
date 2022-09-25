ce417033bfa1b61cc4dbcf96b47f47e9b79d9205
algomachine007 <amaben202phantom@gmail.com>
2022-09-25 18:09:12 +0100

    programmatically updating tsConfig based on desired framework

--- framework/common/config.js
+++ framework/common/config.js
@@ -1,8 +1,10 @@
 const path = require('path');
+const fs = require('fs');
 const merge = require('deepmerge');
 
 // merging 2 framweork configs // deepmerge
 function withFrameworkConfig(defaultConfig = {}) {
+  // @framework would point to another framework programmatically
   const framework = 'shopify';
 
   const frameworkNextConfig = require(path.join(
@@ -12,6 +14,16 @@ function withFrameworkConfig(defaultConfig = {}) {
   ));
 
   const config = merge(defaultConfig, frameworkNextConfig);
+  const tsPath = path.join(process.cwd(), 'tsconfig.json');
+  const tsconfig = require(tsPath);
+
+  tsconfig.compilerOptions.paths['@framework'] = [`@framework/${framework}`];
+  tsconfig.compilerOptions.paths['@framework/*'] = [
+    `@framework/${framework}/*`,
+  ];
+
+  fs.writeFileSync(tsPath, JSON.stringify(tsconfig, null, 2));
+
   return config;
 }
 
