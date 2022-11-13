15c05889b8f72974cdcaecc6605ad655f8d8b36a
algomachine007 <amaben202phantom@gmail.com>
2022-11-13 10:12:47 +0100

    tailwind autoprefixer

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-
+import style from './Layout.module.css';
 interface Children {
   children: React.ReactNode;
 }
 
 const Layout = ({ children }: Children) => {
-  return <div>{children}</div>;
+  return (
+    <main className={style.root}>
+      <div className='fit'>{children}</div>
+    </main>
+  );
 };
 
 export default Layout;
